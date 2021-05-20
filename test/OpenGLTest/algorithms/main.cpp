651cf3fa4876461e48976433c84e94ab016412d2
GRANDSOFT\panl-c <panl-c@glodon.com>
2021-05-20 13:38:21 +0800

    add algorithms class

--- test/OpenGLTest/algorithms/main.cpp
+++ test/OpenGLTest/algorithms/main.cpp
@@ -1,5 +1,7 @@
 
 #include <QtCore/QCoreApplication>
+#include "src/algorithms.h"
+
 
 int main(int argc, char *argv[])
 {
---
+++ test/OpenGLTest/algorithms/src/algorithms.h
@@ -0,0 +1,13 @@
+/**********************************
+*  @description :
+*  @author £ºdivels@qq.com
+*  @date : 2021.05.20
+***********************************/
+
+#ifndef _ALGORITHMS_H_
+#define _ALGORITHMS_H_
+
+
+
+
+#endif // !_ALGORITHMS_H_
